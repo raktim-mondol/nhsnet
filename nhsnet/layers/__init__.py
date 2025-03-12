f7874e81cfc8939d19f06d51e89ecbed46ec6118
Raktim Mondol <raktim.live@gmail.com>
2025-03-13 03:04:41 +1100

    Add layers __init__.py

--- nhsnet/layers/__init__.py
+++ nhsnet/layers/__init__.py
@@ -3,4 +3,9 @@ from .structured_sparse import StructuredSparseConv2d
 from .hh_gating import HodgkinHuxleyGating
 from .dynamic_neurogenesis import DynamicNeurogenesisModule
 
-__all__ = ['HebbianConv2d', 'StructuredSparseConv2d', 'HodgkinHuxleyGating', 'DynamicNeurogenesisModule']
+__all__ = [
+    'HebbianConv2d',
+    'StructuredSparseConv2d',
+    'HodgkinHuxleyGating',
+    'DynamicNeurogenesisModule'
+]
