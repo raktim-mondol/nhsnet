9162fc70ec2e3a8a53805bd54285abfdbfbda002
Raktim Mondol <raktim.live@gmail.com>
2025-03-13 03:00:33 +1100

    Add root __init__.py

--- nhsnet/__init__.py
+++ nhsnet/__init__.py
@@ -1,3 +1,17 @@
-from .models import NHSNet
+from .models.nhsnet import NHSNet
+from .layers.hebbian_conv import HebbianConv2d
+from .layers.structured_sparse import StructuredSparseConv2d
+from .layers.hh_gating import HodgkinHuxleyGating
+from .layers.dynamic_neurogenesis import DynamicNeurogenesisModule
+from .utils.pruning import AdaptiveSynapticPruning
 
 __version__ = '0.1.0'
+
+__all__ = [
+    'NHSNet',
+    'HebbianConv2d',
+    'StructuredSparseConv2d',
+    'HodgkinHuxleyGating',
+    'DynamicNeurogenesisModule',
+    'AdaptiveSynapticPruning',
+]
